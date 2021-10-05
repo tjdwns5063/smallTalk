0bb5c5052a34ac67dea68d447606f4f6cf5b5a82
tjdwns5063@naver.com <tjdwns5063@naver.com>
2021-10-05 17:07:40 +0900

    add bonus header

---
+++ include/minitalk_bonus.h
@@ -0,0 +1,20 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minitalk.h                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seongjki <seongjk@student.42seoul.kr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/09/25 11:33:35 by seongjki          #+#    #+#             */
+/*   Updated: 2021/10/05 15:29:27 by seongjki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINITALK_BONUS_H
+# define MINITALK_BONUS_H
+
+# include <unistd.h>
+# include <signal.h>
+# include "ft_printf.h"
+
+#endif
